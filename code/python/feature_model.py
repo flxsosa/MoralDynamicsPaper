7784ba38e784920645774464d11989c8f6d56787
Felix Sosa <felixanthonysosa@gmail.com>
2021-04-02 14:53:04 -0400

    init commit of feature-based model for moral judgments

---
+++ code/python/feature_model.py
@@ -0,0 +1,41 @@
+'''
+Implementation of feature-based models over the trajectory data
+of simulations.
+
+04/02/2021
+Felix Sosa
+'''
+
+def distance_from_json():
+    '''
+    Takes in a json and outputs the distance the agents traveled
+    in terms of (x,y) units.
+    '''
+    pass
+
+def duration_from_json():
+    '''
+    Takes in a json and outputs the duration of contact between
+    a patient and agent in terms of seconds.
+    '''
+    pass
+
+def contact_from_json():
+    '''
+    Takes in a json and outputs whether the agent collides
+    with patient at all.
+    '''
+    pass
+
+def force_from_json():
+    '''
+    Not sure if this should be implemented.
+    '''
+    pass
+
+def frequency_from_json():
+    '''
+    Takes in a json and outputs number of times agent collides
+    with patient.
+    '''
+    pass
